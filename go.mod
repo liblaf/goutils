530051b8ed8059eb5566e4021e016fb3c54097b2
Qin Li <liblaf@outlook.com>
2023-02-12 10:18:01 +0800

    fix: download progress bar

--- go.mod
+++ go.mod
@@ -6,12 +6,15 @@ require (
 	github.com/pterm/pterm v0.12.54
 	github.com/sirupsen/logrus v1.9.0
 	github.com/spf13/cobra v1.6.1
+	github.com/vbauerster/mpb/v8 v8.1.6
 	mvdan.cc/sh/v3 v3.6.0
 )
 
 require (
 	atomicgo.dev/cursor v0.1.1 // indirect
 	atomicgo.dev/keyboard v0.2.9 // indirect
+	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
 	github.com/containerd/console v1.0.3 // indirect
 	github.com/gookit/color v1.5.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
--- pkg/interactive/download/download.go
+++ pkg/interactive/download/download.go
@@ -1,11 +1,64 @@
 package download
 
 import (
+	"io"
+	"net/http"
+	"os"
+	"sync"
+
 	_errors "github.com/liblaf/goutils/pkg/errors"
 	"github.com/liblaf/goutils/pkg/interactive/confirm"
-	"github.com/liblaf/goutils/pkg/interactive/exec"
 	"github.com/liblaf/goutils/pkg/message"
+	"github.com/vbauerster/mpb/v8"
+	"github.com/vbauerster/mpb/v8/decor"
 )
+
+func doDownload(src string, dst string) {
+	var wg sync.WaitGroup
+	p := mpb.New(mpb.WithWaitGroup(&wg))
+
+	req, err := http.NewRequest(http.MethodGet, src, nil)
+	_errors.Check(err)
+	req.Header.Set("User-Agent", "Mozilla")
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		resp, err := http.DefaultClient.Do(req)
+		_errors.Check(err)
+		defer resp.Body.Close()
+
+		b := p.AddBar(resp.ContentLength,
+			mpb.PrependDecorators(
+				decor.Name(dst, decor.WCSyncSpaceR),
+				decor.Percentage(decor.WCSyncSpaceR),
+				decor.CountersKibiByte("(%.1f/%.1f)", decor.WCSyncSpaceR),
+				decor.Elapsed(decor.ET_STYLE_MMSS, decor.WCSyncSpaceR),
+				decor.OnComplete(
+					decor.AverageETA(decor.ET_STYLE_MMSS, decor.WCSyncSpaceR),
+					"Done.",
+				),
+				decor.AverageSpeed(decor.UnitKiB, "% .2f", decor.WCSyncSpaceR),
+			),
+		)
+		r := b.ProxyReader(resp.Body)
+		defer r.Close()
+
+		out, err := os.Create(dst)
+		_errors.Check(err)
+		defer out.Close()
+
+		n, err := io.Copy(out, r)
+		_errors.Check(err)
+
+		if resp.ContentLength < 0 {
+			b.SetTotal(n, true)
+		}
+	}()
+
+	p.Wait()
+}
 
 func Download(src string, dst string) bool {
 	msg := &message.Message{
@@ -17,7 +70,8 @@ func Download(src string, dst string) bool {
 	if !rsp {
 		return false
 	}
-	c := exec.Command("wget", "--output-document", dst, src)
-	_errors.Check(c.Run())
+
+	doDownload(src, dst)
+
 	return true
 }
